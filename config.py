2ce64d597c0325c9f0dc9a0fef6234f95179f43f
yangchao <chao_china@163.com>
2017-04-07 10:24:52 +0800

    添加一些配置文件简单注释

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
 # coding:utf8
 
+# mysql
 mysql_config = {"host":"127.0.0.1", "port":3306, "user":"ekpapi", "passwd":"ekpapi", "charset": 'utf8', "db":"xserver"}
 
+# mysql table name
 table = "test"
 
+# local service is a slave, need specify a slave server id.
 mysql_server_id = 1
 
+# binlog record
 mysql_binlog_info_file = "binlog_record.log"
 
+# solr
 solr_config = {"schema":"http", "host":"127.0.0.1", "port":8081, "path":"/solr/core0"}
 
+# log file
 file_path = "logger.log"
 
+# mysql columns map solr fields
 mysql_column_solr_index_mapping = {
     "id":"id",
     "name":"name"
