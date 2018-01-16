4ac19fa37143ecb8974438846a5f37a5c0b53f70
YangChao <804647898@qq.com>
2018-01-16 09:44:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # coding:utf8
 
 # mysql
-mysql_config = {"host":"127.0.0.1", "port":3306, "user":"ekpapi", "passwd":"ekpapi", "charset": 'utf8', "db":"xserver"}
+mysql_config = {"host":"127.0.0.1", "port":3306, "user":"root", "passwd":"123456", "charset": 'utf8', "db":"test"}
 
 # mysql table name
 table = "test"
